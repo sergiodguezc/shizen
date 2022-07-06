6409c4e2dbb6fe99340b62a996a6728797d12520
sergiodguezc <sergiodguezc@gmailcom>
2022-07-06 13:00:47 +0200

    AntColony types

--- src/Shizen/AntColony/Main.hs
+++ src/Shizen/AntColony/Main.hs
@@ -10,11 +10,38 @@
 --
 --------------------------------------------------------------------
 
+{-# LANGUAGE FlexibleContexts #-}
+
 module Shizen.AntColony.Main (
-  --   Path
+    -- antColony
   -- , Objective
 ) where
 
 import Data.Array.Accelerate as A
--- import Data.Array.Accelerate.System.Random.MWC
+import Shizen.AntColony.Types
+import Data.Array.Accelerate.System.Random.MWC
+
+-- Function that performs the ant colony algorithm
+-- antColony :: (ObjectiveFunction objectivefn) =>
+-- antColony :: (ObjectiveFunction objectivefn) =>
+--              Int ->                -- Archive size (number of ants)
+--              Int ->                -- Dimension
+--              Double ->             -- MaxBound
+--              ProblemType ->        -- Minimization | Maximization
+--              objectivefn ->  -- Objective function
+--              Double ->             -- Evaporation rate
+--              Double ->             -- Stopping criteria
+--              IO Objective             -- Final value
+-- antColony k n b f e s = do
+--     ants <- initializeAnts k n b
+--     obj <- A.map f.eval ants :: Vector Objective
+--     return $ obj ! 1
+--
+initializeAnts :: Int ->               -- Archive size (number of ants)
+                  Int ->               -- Dimension
+                  Double ->            -- MaxBound
+                  objectivefn ->       -- Objective function
+                  IO (Vector Ant)
+initializeAnts k n b f = do positions <- randomArray (uniformR (-b,b)) (Z :. n :. k) :: IO (Matrix Double)
+                            join generate (index1 n) (\_ -> A.zip (use positions) (use (A.map f.eval positions)))
 
--- src/Shizen/AntColony/Types.hs
+++ src/Shizen/AntColony/Types.hs
@@ -10,39 +10,50 @@
 --
 --------------------------------------------------------------------
 
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Shizen.AntColony.Types (
-    Path
-  , Position
+    Position
   , Objective
   , Ant
+  , ProblemType
+  , ObjectiveFunction
 ) where
 
 import Data.Array.Accelerate as A
--- import Data.Array.Accelerate.System.Random.MWC
+import Data.Array.Accelerate.System.Random.MWC as R
 
 --------------------------------------------------------------------
 --  Basic types to be used 
 --------------------------------------------------------------------
 
+-- | A type of optimization problem: whether the objective function
+-- has to be miminized, or maximized.
+data ProblemType = Minimization | Maximization deriving (Show, Prelude.Eq)
+
 -- | A representation of the exact location in the space (R^n).
 --
 type Position = Vector Double
 
--- | A representation of a solution. 
+-- | A representation of pheromones. 
 --
-type Path = Vector Position
+-- type Pheromones = 
 
 -- | A measure of the observed performance. It may be called cost for
 -- minimization problems, or fitness for maximization problems.
 --
-type Objective = Exp Double
+type Objective = Scalar Double
+
 
 -- | A representation of the ant on each iteration.
 -- 
-type Ant = ()
+type Ant = (Position, Objective)
 
--- | A function that measures the performance of a solution.
+-- | A function to evaluate a genome should be an instance of
+-- 'ObjectiveFunction' class. It may be called a cost function for minimization
+-- problems, or a fitness function for maximization problems.
 --
-class ObjectiveFunction f where
-    evalObjective :: f -> [Path] -> Objective
-
+-- | Evaluate all fitness (cost) values at once.
+--
+class ObjectiveFunction where
+    eval :: Position -> Objective
